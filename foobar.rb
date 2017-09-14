71edd8484237edb155cfe174b526208bc49ae9f4
Naomi Jung <naomi.jung@berkeley.edu>
2017-09-14 00:08:27 -0700

    initial hw1 commit

--- foobar.rb
+++ foobar.rb
@@ -1,9 +1,8 @@
 class Foobar
 
   def self.baz(a)
+    a.uniq{|e| e}.map(&:to_i).map{|e| e+2}.select{|e| e%2 == 0 && e<=10}.inject(:+)
     # Class method
     # Call with `Foobar.baz`
   end
 end
-
-
